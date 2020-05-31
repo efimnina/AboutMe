9b43b2f60eaf80316def33b06c59b9ff64511fab
Nina <ciz0@bk.ru>
2020-05-31 22:57:36 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Нина Ефимова on 31.05.2020.
+//  Copyright © 2020 Efim Nina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
